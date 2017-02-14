91f2489ca85d0fcbccf400c0259e019313c461f1
David Scully <david@expertsoftwaretest.ie>
2017-02-14 16:44:26 +0000

    Authentication with Facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
-import {Observable} from "rxjs";
+import {Component} from "@angular/core";
+import {AngularFire, AuthProviders, AuthMethods} from "angularfire2";
+import {Http} from '@angular/http';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/take";
 
 @Component({
   selector: 'app-root',
@@ -11,53 +11,55 @@ import {Observable} from "rxjs";
 })
 export class AppComponent {
 
-  title = 'app works!';
-  cuisines: FirebaseListObservable<any[]>;
-  restaurant: Observable<any[]>;
-  exists;
+  displayName: string;
+  photoUrl: string;
 
-  constructor(private af: AngularFire) {
+  constructor(private af: AngularFire, private http: Http) {
 
   }
 
   ngOnInit() {
-    this.cuisines = this.af.database.list('/cuisines', {
-      query: {
-        orderByValue: true
+    this.af.auth.subscribe(authState => {
+      if (!authState) {
+        this.displayName = null;
+        this.photoUrl = null;
+        return;
       }
+
+      let userRef = this.af.database.object('/users/' + authState.uid);
+      userRef.subscribe(user => {
+        let url = `https://graph.facebook.com/v2.8/${authState.facebook.uid}?fields=first_name,last_name,gender&access_token=${user.accessToken}`;
+        this.http.get(url).subscribe(response => {
+          let user = response.json();
+          userRef.update({
+            firstName: user.first_name,
+            lastName: user.last_name
+          });
+        });
+      });
+
+
+      this.displayName = authState.auth.displayName;
+      this.photoUrl = authState.auth.photoURL;
     });
-    this.restaurant = this.af.database.list('/restaurants', {
-      query: {
-        orderByChild: 'rating',
-        startAt: 3,
-        endAt: 5,
-        limitToFirst: 3
-      }
-    });
   }
 
-  add() {
-    this.af.database.list('/restaurants').push({name: ""})
-      .then(x => {
-        // x.key
-        let restaurant = {name: 'My new restaurant'};
-        let update = {};
-        update['restaurants/' + x.key] = restaurant;
-        update['restaurants-by-city/camberwell/' + x.key] = restaurant;
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup,
+      scope: ['email', 'public_profile', 'user_friends']
+    }).then((authState: any) => {
+      this.af.database.object('/users/' + authState.uid).update({
+        accessToken: authState.facebook.accessToken
+      });
+    })
 
-        this.af.database.object('/').update(update);
-      })
-      .catch(error => console.log(error));
   }
 
-  update() {
-    this.af.database.object('/favourites/1/10').set(null);
-  }
 
-  remove() {
-    this.af.database.object('/restaurant').remove()
-      .then(x => console.log("SUCCESS"))
-      .catch(error => console.log(error));
+  logout() {
+    this.af.auth.logout();
   }
 
 }
